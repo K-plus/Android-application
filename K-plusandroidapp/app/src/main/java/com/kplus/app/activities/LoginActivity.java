04eca8d1fb47f14fd001294c7a0c01a9ae8521a4
Vasco de Krijger <vascodekrijger@gmail.com>
2015-01-21 14:12:44 +0100

    setup van de libraries

---
+++ K-plusandroidapp/app/src/main/java/com/kplus/app/activities/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.kplus.app.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.kplus.android.k_plusandroidapp.R;
+
+
+public class LoginActivity extends Activity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
