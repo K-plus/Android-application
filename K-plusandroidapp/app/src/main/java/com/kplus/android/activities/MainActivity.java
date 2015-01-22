1b5b6652aaba2ae408e808404eb1d85280f6235b
Vasco de Krijger <vascodekrijger@gmail.com>
2015-01-22 13:13:31 +0100

    Begin gemaakt met het scannen van de barcodes Het productnummer wordt verder nog neit verwerkt

--- K-plusandroidapp/app/src/main/java/com/kplus/android/activities/MainActivity.java
+++ K-plusandroidapp/app/src/main/java/com/kplus/android/activities/MainActivity.java
@@ -1,34 +1,69 @@
 package com.kplus.android.activities;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
 import com.kplus.android.k_plusandroidapp.R;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity
+{
+    private final int SCAN_BARCODE = 1;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
+    private void scanBarcode()
+    {
+        try
+        {
+            Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+            intent.putExtra("SCAN_MODE", "PRODUCT_MODE");
+            startActivityForResult(intent, SCAN_BARCODE);
+        }
+        catch (Exception e)
+        {
+            //Als hij niet geinstalleerd is open de markt om hem te installeren
+            Uri marketUri = Uri.parse("market://details?id=com.google.zxing.client.android");
+            Intent marketIntent = new Intent(Intent.ACTION_VIEW,marketUri);
+            startActivity(marketIntent);
+        }
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent)
+    {
+        if (requestCode == SCAN_BARCODE && resultCode == RESULT_OK)
+        {
+            String contents = intent.getStringExtra("SCAN_RESULT");
+            String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
+
+            Toast toast = Toast.makeText(this, "Content:" + contents + " Format:" + format , Toast.LENGTH_LONG);
+            toast.show();
+        }
+    }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_scanproduct)
+        {
+            scanBarcode();
             return true;
         }
         return super.onOptionsItemSelected(item);
